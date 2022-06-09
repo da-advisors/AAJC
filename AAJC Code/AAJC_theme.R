556b58f75ba98210f3f113dabf8fd70767d25451
akaAnam <ak1963@georgetown.edu>
2022-06-09 16:19:55 -0400

    Map code

---
+++ AAJC Code/AAJC_theme.R
@@ -0,0 +1,128 @@
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+library(scales)
+library(showtext)
+library(stringr)
+
+
+## THEME IDEAS ## 
+## Based off examples from: 
+## https://www.advancingjustice-aajc.org/sites/default/files/2019-07/1153_AAJC_Immigration_Final_0.pdf
+
+# TITLE FONT - Open Sans bold
+#     color - black
+# SUBTITLE FONT - Open Sans bold
+#     color - black
+# Y-AXIS FONT - ie. the countries
+#     color - gray
+
+# AAJC Colors: 
+# -----------
+# Color picked from sample report pdf using a color picker: 
+# • Red - #EF6D59
+
+# • Orange - #E37232
+#     • 80% tint --> #f9e3d6 (MOST READABLE)
+
+# • Purple - #3F3875
+#     might want to get rid of the red. Makes the map pretty unclear or keep just one
+#     color like orange for a state by state analysis 
+
+red <- '#EF6D59'
+orange <- '#E37232'
+orange_80t <- '#f9e3d6'
+purple <- '#3F3875'
+
+
+# Importing the font: 
+font_add(family = "Open Sans", "OpenSans-Bold.ttf") 
+showtext_auto()
+
+
+# CREATING THEME 
+# --------------
+#   building off theme_void() - its almost exactly what we need (i think) 
+
+theme_aajc <- theme_void() +
+  theme(
+    # Similar font to AAJC report &
+    # positioning Title, subtitle, and Legend away from map for Alysha
+    plot.title = element_text(family = "Open Sans",
+                              face = "bold",
+                              vjust = 5,
+                              size=15),
+    plot.subtitle = element_text(vjust = 9),
+    
+    # making legend text match with report example 
+    legend.title = element_text(family = "Open Sans",
+                                face = "bold",
+                                size = 8),
+    
+    # increasing space between legend and map 
+    legend.margin = margin(10,10,10,10),
+    legend.box.margin = margin(10,10,10,10))
+
+# Save theme as RDS for future use 
+theme_aajc %>% saveRDS('theme_AAJC.rds')
+
+# Remove and re-add saved theme (sanity check)
+rm(theme_aajc)
+theme_AAJC <- readRDS('theme_AAJC.rds')
+
+
+
+
+# Function to uppercase title and subtitles on ggplots
+#   could not find a way to bake this into the custom theme 
+#   Will have to include source("AAJC_theme.R") with library imports 
+
+ggplot_add.my_theme <- function(object, plot, object_name){
+  # plot$theme <- ggplot2:::update_theme(plot$theme, object)
+  plot$labels$title <- stringr::str_to_upper(plot$labels$title)
+  plot$labels$subtitle <- stringr::str_to_upper(plot$labels$subtitle)
+  plot
+}
+
+titles_upper <- function(){
+  out <- theme_AAJC
+  
+  class(out) <- c("my_theme", class(out))
+  
+  return(out)
+}
+
+
+# Theme trial & error   
+AA_alone_2000_MAP <- AA_alone %>% 
+  mutate(percent = 100 * (value/summary_value)) %>%
+  ggplot(aes(fill = percent)) + 
+  geom_sf(color = "white") +
+  theme_AAJC + 
+  scale_fill_gradient(low = orange_80t, high = orange) + 
+  labs(fill = "% of Asian     \npopulation     ",
+       title = str_to_upper(
+         "     Percentage of Asian Alone Population"
+       ),
+       subtitle =  str_to_upper("       2000 Decennial Census"))
+
+
+AA_alone_2000_MAP
+
+
+# AAJC Color Fill: 
+
+# Me testing some stuff : 
+
+# Only orange gradient (better for state by state analysis)
+# AA_alone_2000_MAP + 
+#   scale_fill_gradient(low = "#fcf1eb", high = "#E37232") + 
+#   theme(
+#     plot.title = element_text(family = "Open Sans", face = "bold", size=15)
+#   )     # most readable
+# 
+# # Orange to purple 
+# AA_alone_2000_MAP + 
+#   scale_fill_gradientn(colours = c("#e68047", "#524c83"))
+
+
---
+++ AAJC Code/AA_NHPI_STATE_analysis.R
@@ -0,0 +1,178 @@
+library(tidycensus)
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+library(tigris)
+library(mapproj)
+library(maps)
+source("AAJC_theme.R") # for titles_upper() - applies capitalization to all titles of ggplot object
+
+
+# NEEDED VIS: 
+# 1. [Map] Decennial Census: Asian Alone population by state count / percent
+#           of total pop (2000, 2010, 2020)
+
+
+
+# Setting up THEME, COLORS, & API 
+# -------------------------------
+
+# Anam's Key: 
+census_api_key("0d3f6eaad6d4d9ffb24d6b420e4deccd7fe7f780")
+
+# Import theme created for AAJC Analysis in "AAJC Code/AAJC_theme.R"
+theme_AAJC <- readRDS('theme_AAJC.rds')
+
+# Defining color variables (see AAJC_them.R for info on these colors)
+red <- '#EF6D59'
+orange <- '#E37232'
+orange_80t <- '#f9e3d6'
+orange_10t <- "#e68047"
+purple <- '#3F3875'
+
+
+#################
+## ASIAN ALONE ##
+#################
+
+# Pulling data  
+# ------------
+
+# 2000
+# ----
+
+# Loading all vars and searching for the needed ones
+d2000 <- load_variables(2000, 'sf1', cache = T)
+
+# Whats the diff between 'Total!!Asian alone' and 'Total!!Population of one race!!Asian alone'
+#     They are the same values - only need one
+#     P007005 vs P003006
+#     Total population for RACE [8] - P007005 --> P007001
+
+options(tigris_use_cache = TRUE)
+
+# Needed variables 
+totPop <- "P007001"
+asian_alone <- "P007005" 
+# total races tallied!!Asian alone or in combination with one or more other races
+asian_alone_or_combination <- "P009005" 
+
+# Total!!Native Hawaiian and Other Pacific Islander alone
+nhpi_alone <- "P007006"
+
+AA_alone_2000 <- 
+  get_decennial(
+  geography = "state",
+  variables = nhpi_alone,
+  geometry = TRUE,
+  resolution = "20m",
+  summary_var = totPop,  # total population in this instance 
+  year = 2000) %>%
+  shift_geometry()
+
+head(AA_alone_2000)
+
+# MAP 
+# ---
+AA_alone_2000_MAP <- AA_alone_2000 %>% 
+  mutate(percent = 100 * (value/summary_value)) %>%
+  ggplot(aes(fill = percent)) + 
+  geom_sf(color = "white") +
+  theme_AAJC + 
+  scale_fill_gradient(low = orange_80t, high = orange) + 
+  labs(fill = "% of NHPI     \npopulation     ",
+       title ="     Native Hawaiian and Other Pacific Islander Population",
+       subtitle = "       2000 Decennial Census") +
+  titles_upper()
+
+
+
+# ----
+# 2010
+# ----
+
+# Loading all vars and searching for the needed ones
+d2010 <- load_variables(2010, 'sf1', cache = T)
+
+# total races tallied!!Asian alone or in combination with one or more other races
+asian_alone_or_combination <- "P006005"
+
+# Total!!Native Hawaiian and Other Pacific Islander alone
+nhpi_alone <- "P003006"
+
+AA_alone_2010 <- 
+  get_decennial(
+    geography = "state",
+    variables = nhpi_alone,
+    geometry = TRUE,
+    resolution = "20m",
+    summary_var = "P003001",  # total population in this instance 
+    year = 2010) %>%
+  shift_geometry()
+
+head(AA_alone_2010)
+
+# MAP
+# ---
+AA_alone_2010_MAP <- AA_alone_2010 %>% 
+  mutate(percent = 100 * (value/summary_value)) %>%
+  ggplot(aes(fill = percent)) + 
+  geom_sf(color = "white") + 
+  scale_fill_gradient(low = orange, high = purple) +
+  labs(fill = "% of NHPI     \npopulation     ",
+       title ="     Native Hawaiian and Other Pacific Islander Population",
+       subtitle = "         2010 Decennial Census") +
+  titles_upper() +
+  theme_AAJC
+
+
+
+# ----
+# 2020
+# ----
+
+# Loading all vars and searching for the needed ones
+d2020 <- load_variables(2020, 'pl', cache = T)
+
+# !!Total:!!Population of one race:!!Native Hawaiian and Other Pacific Islander alone
+nhpi_alone <- "P1_007N"
+
+AA_alone_2020 <- 
+  get_decennial(
+    geography = "state",
+    variables = nhpi_alone,
+    geometry = TRUE,
+    resolution = "20m",
+    summary_var = "P1_001N",  # total population in this instance 
+    year = 2020) %>%
+  shift_geometry()
+
+head(AA_alone_2020)
+
+# MAP
+# ---
+AA_alone_2020_MAP <- AA_alone_2020 %>% 
+  mutate(percent = 100 * (value/summary_value)) %>%
+  ggplot(aes(fill = percent)) + 
+  geom_sf(color = "white") + 
+  scale_fill_gradient(low = orange_80t, high = orange) + 
+  labs(fill = "% of NHPI     \npopulation     ",
+       title ="     Native Hawaiian and Other Pacific Islander Population",
+       subtitle = "         2020 Decennial Census") +
+  titles_upper() +  # function to uppercase titles 
+  theme_AAJC
+
+
+# Preview maps 
+AA_alone_2000_MAP
+AA_alone_2010_MAP
+AA_alone_2020_MAP
+
+
+# Save into Vis Folder
+ggsave("../AAJC Vis/NHPI_alone_2000_STATE_MAP_one_color.png",
+       plot = AA_alone_2000_MAP, bg = "white")
+ggsave("../AAJC Vis/NHPI_alone_2010_STATE_MAP_one_color.png",
+       plot= AA_alone_2010_MAP, bg="white")
+ggsave("../AAJC Vis/NHPI_alone_2020_STATE_MAP_one_color.png",
+       plot= AA_alone_2020_MAP, bg="white")
