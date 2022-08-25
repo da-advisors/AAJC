b065cdb45a017e540d1c69ab26e564e3a74a9c06
alysha-21 <63248033+alysha-21@users.noreply.github.com>
2022-08-25 03:06:12 -0400

    color palette options for map
    all are okay for color blindness

---
+++ Transformed Data/data for viz_alysha/Map Color Palettes.R
@@ -0,0 +1,8 @@
+install.packages("rcartocolor")
+library(rcartocolor)
+display_carto_all(colorblind_friendly = TRUE)
+
+display_carto_pal(7, "PurpOr") #state maps
+display_carto_pal(7, "SunsetDark") #county maps / tables
+display_carto_pal(7, "Sunset") #county maps
+display_carto_pal(7, "OrYel") #state maps, option 2
